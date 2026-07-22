392aeae89bad8abc614d8a8ab0422042d6402b78
ADITI MISHRA <aditi.25b01011183@abes.ac.in>
2026-07-22 11:18:58 +0530

    q2

--- 22_07.js
+++ 22_07.js
@@ -1,14 +1,43 @@
 // write a funtion to take numner b/w 1-9 and return in words
 
-function numbertowords(num){
-    const words = ["one","two","three","four","five","six","seven","eight","nine"];
-    if(num>=1&&num<=9){
-        return words[num-1];
+// function numbertowords(num){
+//     const words = ["one","two","three","four","five","six","seven","eight","nine"];
+//     if(num>=1&&num<=9){
+//         return words[num-1];
 
-    }else{
-        return "enter a number";
+//     }else{
+//         return "enter a number";
+//     }
+// }
+// console.log(numbertowords(5));
+
+
+const towords = (digit)=>{
+    switch(digit){
+        case 1:
+            return "one";
+        case 2:
+            return "two";
+        case 3:
+            return "three";
+        case 4:
+            return "four";
+        case 5:
+            return "five";
+        case 6:
+            return "six";
+        case 7:
+            return "seven";
+        case 8:
+            return "eight";
+        case 9:
+            return "nine";
+        default:
+            return "enter numb";
     }
 }
-console.log(numbertowords(5));
+console.log(towords(5));
+
+// create another funtion that takes a number and show in words with the help of towords funtion.
 
 
